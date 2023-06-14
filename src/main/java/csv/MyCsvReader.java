c66313da32bc67fac43e70aa8169c4544ca6e000
peterwmef <94199457+peterwmef@users.noreply.github.com>
2023-06-14 15:09:45 -0400

    InputStream works

--- src/main/java/csv/MyCsvReader.java
+++ src/main/java/csv/MyCsvReader.java
@@ -2,36 +2,46 @@ package csv;
 
 import util.U;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.nio.charset.StandardCharsets;
+import java.io.*;
 
+@SuppressWarnings({"FieldCanBeLocal", "unused"})
 public class MyCsvReader { // class /////////////////////////////////////
     final private String csvFilePath;
-//    final private String eolChar;
+    final private String resultFilePath;
+    final private String eolChar;
+    final private int    maxByteCount;
+          private int    currByteCount;
 
-@SuppressWarnings("unused")
-public MyCsvReader(String csvFilePath, String eolChar) {
-    this.csvFilePath = csvFilePath;
-//    this.eolChar = eolChar;
+public MyCsvReader(String csvFilePath, String eolChar, int maxByteCount, String resultFilePath) {
+    this.csvFilePath    = csvFilePath;
+    this.eolChar        = eolChar;
+    this.maxByteCount   = maxByteCount;
+    this.currByteCount  = 0;
+    this.resultFilePath = resultFilePath;
 }
 
-@SuppressWarnings("UnnecessaryContinue")
 public void readAndPrint() throws Exception {
-    BufferedReader reader = new BufferedReader(new FileReader(csvFilePath, StandardCharsets.UTF_8));
-    while (true) {
-        int c = reader.read();
-        if (c == -1) break;
-        if (c == 13) {
-            continue;
-        } else if (c == 10) {
-            U.print("___EOL___");
-            continue;
-        } else {
-            U.printInline(c + "[" + (char)c + "]");
+
+    InputStream is1 = new FileInputStream(csvFilePath); // InputStream is used to read binary data, while Reader is used to read text data, precisely Unicode characters.
+                                                        // InputStream.read() returns -1, or 0-255.  Nothing else. (Don't worry about 2's complements)
+    BufferedInputStream bis = new BufferedInputStream(is1);
+    StringBuilder s = new StringBuilder();
+
+
+    try (FileOutputStream output = new FileOutputStream(resultFilePath, true)) { // Writers write characters, while OutputStreams write bytes
+
+        while (true) {
+            int b;
+            if (   currByteCount++ > maxByteCount || (b = bis.read()) == -1   )  break;
+            switch(b) {
+                case 10: U.print("___EOL___"); // LF
+                case 13: continue; // CR - eol is "CR-LF" or "LF"
+                default:
+                    U.printInline(b + "[" + (char)b + "]");
+                    //                output.write(c); // String.format(""0x%04X "", c)
+            }
+
         }
-
-
 
     }
 
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,7 +7,7 @@ public static void main(String[] args) throws Exception {
     System.out.println("===== usage: java -jar jarFileName.jar csvFilePath EOL_Char[CR_LF|LF] ");
 //    BufferedReader reader = new BufferedReader(new FileReader(args[0], StandardCharsets.UTF_8));
 //    int c = reader.read();
-    MyCsvReader myCsvReader = new MyCsvReader(args[0], args[1]);
+    MyCsvReader myCsvReader = new MyCsvReader(args[0], args[1], 10000, "2___result/r1.csv");
     myCsvReader.readAndPrint();
 
 }
