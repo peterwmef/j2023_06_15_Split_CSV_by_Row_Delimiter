10cace801925d5661a1108ad53284b8142ea826a
peterwmef <94199457+peterwmef@users.noreply.github.com>
2023-06-14 12:35:36 -0400

    added scope and usage instruction

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
+
 public class Main { // class ///////////////////////////////////////
 
-public static void main(String[] args) {
-    System.out.println("Hello world!");
+public static void main(String[] args) throws Exception {
+    System.out.println("===== usage: java -jar jarFileName.jar csvFilePath EOL_Char");
+//    BufferedReader reader = new BufferedReader(new FileReader(args[0], StandardCharsets.UTF_8));
+//    int c = reader.read();
+
 }
 
 } // class /////////////////////////////////////////////////////
