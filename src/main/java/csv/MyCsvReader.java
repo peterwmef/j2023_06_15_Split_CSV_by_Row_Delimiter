aaafb0a716a82a64e96f29da7ee133ee0e3a1e0d
peterwmef <94199457+peterwmef@users.noreply.github.com>
2023-06-14 15:36:40 -0400

    . + printToFile() . refactor main loop

--- src/main/java/csv/MyCsvReader.java
+++ src/main/java/csv/MyCsvReader.java
@@ -1,12 +1,12 @@
 package csv;
-
 import util.U;
 import util.UFile;
-
 import java.io.*;
 
-@SuppressWarnings({"FieldCanBeLocal", "unused"})
+@SuppressWarnings({"FieldCanBeLocal", "unused", "UnnecessaryLocalVariable"})
 public class MyCsvReader { // class /////////////////////////////////////
+    final private boolean printToFile = false;
+
     final private String csvFilePath;
     final private String resultFilePath;
     final private String eolChar;
@@ -22,37 +22,47 @@ public MyCsvReader(String csvFilePath, String eolChar, int maxByteCount, String 
 }
 
 public void readAndPrint() throws Exception {
-    UFile.deleteFileOrDir(resultFilePath);
-//    UFile.createFile     (resultFilePath);
+    UFile.deleteFileOrDir(resultFilePath);     //    UFile.createFile     (resultFilePath);
+    BufferedInputStream bis = getBufferedInputStream();
 
-    InputStream is1 = new FileInputStream(csvFilePath); // InputStream is used to read binary data, while Reader is used to read text data, precisely Unicode characters.
-                                                        // InputStream.read() returns -1, or 0-255.  Nothing else. (Don't worry about 2's complements)
-    BufferedInputStream bis = new BufferedInputStream(is1);
-    StringBuilder s = new StringBuilder();
-
-
-    try (FileOutputStream output = new FileOutputStream(resultFilePath, true)) { // Writers write characters, while OutputStreams write bytes
+    try (FileOutputStream fos = new FileOutputStream(resultFilePath, true)) { // Writers write characters, while OutputStreams write bytes
 
         while (true) {
-            int b;
-            if (   currByteCount++ > maxByteCount || (b = bis.read()) == -1   )  break;
-            switch(b) {
-                case 10:
-//                    U.print("___EOL___"); // LF
-                    output.write('\n');
-                case 13:
-                    continue; // CR - eol is "CR-LF" or "LF"
-                default:
-//                    U.printInline(b + "[" + (char)b + "]");
-                    output.write( (char) b);
-                    //                output.write(c); // String.format(""0x%04X "", c)
-            }
-
+            int b = bis.read();
+            if (currByteCount++ > maxByteCount || b == -1) break; // eol is "CR-LF" or "LF"
+            passChar(fos, b);
         }
 
     }
 
 
 }
+
+private BufferedInputStream getBufferedInputStream() throws FileNotFoundException {
+    InputStream is1 = new FileInputStream(csvFilePath); // InputStream is used to read binary data, while Reader is used to read text data, precisely Unicode characters.
+                                                        // InputStream.read() returns -1, or 0-255.  Nothing else. (Don't worry about 2's complements)
+    BufferedInputStream bis = new BufferedInputStream(is1);
+    return bis;
+}
+
+private void passChar(FileOutputStream fos, int b) throws Exception {
+    switch(b) {
+        case 10: // LF
+            printInlineByte('\n', fos); // LF
+        case 13: // CR
+            return;
+        default:
+            printInlineByte(b, fos);
+    }
+}
+
+public void printInlineByte(int b, FileOutputStream fos) throws Exception {
+    if (printToFile) {
+        fos.write( (char) b); // String.format(""0x%04X "", c)
+    } else {
+        U.printInline("[" + b + "]" + (char)b );
+    }
+}
+
 
 } // class /////////////////////////////////////
