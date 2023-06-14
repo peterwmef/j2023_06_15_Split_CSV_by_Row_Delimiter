4336dc59802caad1044205a74c2971c9aff0c18a
peterwmef <94199457+peterwmef@users.noreply.github.com>
2023-06-14 16:39:00 -0400

    make this repo obsolete, until i have time to review text qualifiers, and nested text qualifiers.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,6 +4,19 @@ import csv.MyCsvReader;
 public class Main { // class ///////////////////////////////////////
 
 public static void main(String[] args) throws Exception {
+
+
+
+
+    // This repo is obsolete.
+    // I don't have time to implement text qualifiers surrounding each value, such as double quote or @#$.
+
+
+
+
+
+
+
     System.out.println("===== usage: java -jar jarFileName.jar csvFilePath EOL_Char[CR_LF|LF] ");
 //    BufferedReader reader = new BufferedReader(new FileReader(args[0], StandardCharsets.UTF_8));
 //    int c = reader.read();
