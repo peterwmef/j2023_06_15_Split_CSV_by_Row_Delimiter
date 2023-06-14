4f11c6d4ea90c54a851e2b1c894c9ff8cce54a24
peterwmef <94199457+peterwmef@users.noreply.github.com>
2023-06-14 12:19:35 -0400

    1st files

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main { // class ///////////////////////////////////////
+
+public static void main(String[] args) {
+    System.out.println("Hello world!");
+}
+
+} // class /////////////////////////////////////////////////////
